42010b6393df0b01948aa59602b1f65fd43b2d01
xrsrke <xrsrke@users.noreply.github.com>
2022-11-24 07:36:13 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="xrsrke"
+original_name="fastops"
+original_urlname="fastops"
+original_description="Awesome fastops created by xrsrke"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv fastops $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
